695e0debf6f4b095b2077a4a75c81954bb63c211
Ak-Rajak <atulkumar.coderguys@gmail.com>
2023-11-05 00:12:27 +0530

    Modifying the UI code

---
+++ 2048.py
@@ -0,0 +1,15 @@
+from tkinter import Frame,Label,CENTER
+
+import Logics
+import constants as c 
+
+
+class Game2048(Frame):
+    def __init__(self):
+
+        self.grid()
+        self.title('2048')
+        self.master.bind("<Key>",self.key_down)
+        self.commands = {c.KEY_UP:Logics.move_up, c.KEY_DOWN : Logics.move_down,
+                         c.KEY_LEFT: Logics.move_left , c.KEY_RIGHT : Logics.move_right
+        }
