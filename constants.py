cbe31816bf36b3d0d77deed2a64089a2ac48bb79
Ak-Rajak <atulkumar.coderguys@gmail.com>
2023-11-04 23:35:24 +0530

    Add constants in file

---
+++ constants.py
@@ -0,0 +1,37 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#CDC0B0"
+BACKGROUND_COLOR_CELL_EMPTY = "#EEDFCC"
+
+BACKGROUND_COLOR_DICT = {2:"#FFEFDB",
+                         4:"#EED5B7",
+                         8:"#FF7D40",
+                         16:"#FF7F24",
+                         32:"#FF4040",
+                         64:"#FF4500",
+                         128:"#FFA500",
+                         256:"#FFD700",
+                         512:"#FFD700",
+                         1024:"#FFD700",
+                         2048:"#FFD700"} 
+
+CELL_COLOR_DICT = {2:"#F8F8FF",
+                    4:"#F8F8FF",
+                    8:"#F8F8FF",
+                    16:"#F8F8FF",
+                    32:"#F8F8FF",
+                    64:"#F8F8FF",
+                    128:"#F8F8FF",
+                    256:"#F8F8FF",
+                    512:"#F8F8FF",
+                    1024:"#F8F8FF",
+                    2048:"#F8F8FF"} 
+
+FONT = {"Verdana" , 40 ,"bold"}
+
+KEY_UP = "'w'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
+KEY_RIGHT ="'d'"
