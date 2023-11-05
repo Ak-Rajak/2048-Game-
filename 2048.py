7554e0e948a4cc15e2cb174ea46c70094e9c2639
Ak-Rajak <atulkumar.coderguys@gmail.com>
2023-11-05 23:22:10 +0530

    Mofiying the main class

--- 2048.py
+++ 2048.py
@@ -12,4 +12,14 @@ class Game2048(Frame):
         self.master.bind("<Key>",self.key_down)
         self.commands = {c.KEY_UP:Logics.move_up, c.KEY_DOWN : Logics.move_down,
                          c.KEY_LEFT: Logics.move_left , c.KEY_RIGHT : Logics.move_right
-        }
+                        }
+
+        self.grid_cells = []
+        self.init_grid()
+        self.init_matrix()
+        self.update_grid_cells()
+
+        self.mainloop()
+
+    def init_grid(self):
+         
